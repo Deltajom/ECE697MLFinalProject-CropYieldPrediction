b63cb0afc8c82e4d730e10e6bcfb45f05edb944e
Deltajom <murray.john.o216@gmail.com>
2023-05-18 20:08:16 -0400

    small edits

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import re
+import os
 import sys
 import logging
 import numpy as np
 from datetime import datetime
 
+resdir = "Results/"+str(datetime.now().strftime("%m-%d-%Y-%H-%M-%S"))
 
-logging.basicConfig(filename='local.log', encoding='utf-8', level=logging.DEBUG)
+os.mkdir(resdir)
+logging.basicConfig(filename=resdir+'/local_run.log', encoding='utf-8', level=logging.DEBUG)
 #logging.disable('DEBUG') # If this line is uncommented, logging will occur, otherwise nothing will be logged
 logging.debug('\n\n\n\n--------------------STARTING NEW RUN - '+ str(datetime.now()) + ' --------------------')
+
 
 # ------------------- CUDA CHECK SECTION -------------------
 useCUDA = True # CHANGE THIS TO FALSE TO USE TORCH CPU
@@ -24,10 +29,27 @@ else:
 logging.debug(testtorch.test_version())
 # ----------------------------------------------------------
 
+
 import pandas as pd
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, ConcatDataset
 from sklearn.model_selection import KFold
+
+
+#print(torch.randn(4, 3, 5))
+
+# R  = Rainfall
+# P = pesticides
+# T = Temperature
+
+# [ R, P, T ]
+
+#[
+# [ R, P, T ]
+# [ P, R, T ]     Possible input modifications
+# [ T, R, P ] 
+# ]
+
 
 mode="???"
 
@@ -57,7 +79,7 @@ class LoadDataset1(Dataset):
                 # TODO - Domenic, add the case where any crop, for any year, for a country in countries makes up the dataset
                 print("to impliment")
             else:
-                self.datain = self.datain[(self.datain[1].isin(countries)) & (self.datain[2].isin(crops))].reset_index().drop(columns=['index'], axis=1)
+                self.datain = self.datain[(self.datain[1].isin(countries)) & (self.datain[2].isin(crops))].reset_index().drop(columns=['index'], axis=1).set_axis(range(self.datain.shape[1]), axis=1)
         else:
             if crops == "all":
                 # TODO - Domenic, add the case where any crop, for any set of years, for a country in countries makes up the dataset
@@ -68,7 +90,7 @@ class LoadDataset1(Dataset):
                     (self.datain[1].isin(countries)) &
                     (self.datain[2].isin(crops)) &
                     (self.datain[3].isin(years))
-                ].reset_index().drop(columns=['index'], axis=1)
+                ].reset_index().drop(columns=['index'], axis=1).set_axis(range(self.datain.shape[1]), axis=1)
 
         logging.debug("Parsed Input Dataset \n" + str(self.datain) + "\n")
 
@@ -114,26 +136,36 @@ class LoadDataset1(Dataset):
 
         # endregion Oldstuff
 
+class RMSELoss(torch.nn.Module):
+    def __init__(self):
+        super(RMSELoss,self).__init__()
 
+    def forward(self,x,y):
+        MSE_loss = nn.MSELoss()
+        RMSE_loss = torch.sqrt(MSE_loss(x, y))
+        return RMSE_loss
 
 class CropYieldPredictionModel(nn.Module):
     def __init__(self):
         super().__init__()
-        # After super initialize model structures
-        self.layers = nn.Sequential(
-        nn.Conv1d(3, 6, 5),
-        nn.MaxPool2d(2, 2),
-        nn.Conv1d(6, 12, 5),
-        nn.ReLU(),
-        nn.Linear(16 * 5, 120),
-        nn.Linear(120, 84),
-        nn.Linear(84, 10),
-        nn.Linear(10, 1)
-        )
+        self.conv1 = nn.Conv1d(in_channels=1, out_channels=420, kernel_size=3, bias=True).cuda()
+        self.zeropad2 = nn.ZeroPad2d((1, 1, 0, 0)).cuda()
+        self.conv2 = nn.Conv1d(in_channels=420, out_channels=120, kernel_size=3, bias=True).cuda()
+        self.conv3 = nn.Conv1d(in_channels=120, out_channels=60, kernel_size=3, bias=True).cuda()
+        self.conv4 = nn.Conv1d(in_channels=60, out_channels=1, kernel_size=3, bias=True).cuda()
 
     # Provide model path
     def forward(self, x):
-        return self.layers(x)
+        #print("Layer 1 input" + str(x))
+        x = self.zeropad2(torch.nn.functional.relu(self.conv1(x)))
+        #print("Layer 2 input" + str(x))
+        x = self.zeropad2(torch.nn.functional.relu(self.conv2(x)))
+        #print("Layer 3 input" + str(x))
+        x = self.zeropad2(torch.nn.functional.relu(self.conv3(x)))
+        #print("Layer 4 input" + str(x))
+        x = torch.nn.functional.relu(self.conv4(x))
+        #print(x)
+        return x
 
 
 
@@ -142,72 +174,95 @@ if __name__ == "__main__":
     
     # K-folds cross-validation implimentation referenced from
     # https://github.com/christianversloot/machine-learning-articles/blob/main/how-to-use-k-fold-cross-validation-with-pytorch.md
-    k_folds = 5
+    k_folds = 4
     test_split = 0.15
-    training_epochs = 40
+    training_epochs = 400
     results = {}
     torch.manual_seed(420) # Might want to comment this, as it will have an effect on k-folds data
-    loss_function = torch.nn.MSELoss() # torch.sqrt() for RMSE
+    loss_function = RMSELoss()
 
-    DS = LoadDataset1(['Albania'], "all", ["Maize"])
+    DS = LoadDataset1(['Bahamas', 'Bangladesh', 'Brazil','Guatemala','Germany'], "all", ["Maize"])
     # print(DS.__getitem__(5))
+
+    # region TODO - MOVE INTO DATASET1 CLASS
 
     training_set, test_set =  np.split(DS.datain.sample(frac=1, random_state=68), [int((1.0-test_split)*len(DS))])
     training_set = training_set.reset_index().drop(columns=['index'], axis=1).set_axis(range(training_set.shape[1]), axis=1)
     test_set     = test_set.reset_index().drop(columns=['index'], axis=1).set_axis(range(test_set.shape[1]), axis=1)
-    logging.debug("Training Set \n" + str(training_set) + "\n")
-    logging.debug("Test Set \n" + str(test_set) + "\n")
+    logging.debug("Training Set with labels \n" + str(training_set) + "\n")
+    logging.debug("Test Set with labels \n" + str(test_set) + "\n")
+
+    training_set = training_set.reset_index().drop(columns=[0,1,2,'index'], axis=1)
+    test_set     = test_set.reset_index().drop(columns=[0,1,2, 'index'], axis=1)
+    training_set = training_set.set_axis(range(training_set.shape[1]), axis=1)
+    test_set     = test_set.set_axis(range(test_set.shape[1]), axis=1)
+    logging.debug("Training Set raw \n" + str(training_set) + "\n")
+    logging.debug("Test Set raw \n" + str(test_set) + "\n")
+
+    training_set_xs = training_set[[training_set.columns[1],training_set.columns[2],training_set.columns[3]]].to_numpy().astype(np.float32)
+    training_set_ys = training_set[training_set.columns[0]].to_numpy().astype(np.float32)
+    # region Expanded Loop now uses list comnprehention
+    trainingdict = {}
+    # for row in range(0,len(training_set_xs)):
+    #     trainingdict[row] = {"x": training_set_xs[row], "y":training_set_ys[row]}
+    # endregion Expanded Loop now uses list comnprehention
+    trainingsetdict = {row: {"x": training_set_xs[row], "y": training_set_ys[row]} for row in range(len(training_set_xs))}
+    logging.debug("Training Set dictionary \n" + re.sub("(.{82})", "\\1\n", str(trainingsetdict), 0, re.DOTALL) + "\n")
+    test_set_xs     = test_set[[test_set.columns[1],test_set.columns[2],test_set.columns[3]]].to_numpy().astype(np.float32)
+    test_set_ys     = test_set[test_set.columns[0]].to_numpy().astype(np.float32)
+    testsetdict     = {row: {"x": test_set_xs[row], "y": test_set_ys[row]} for row in range(len(test_set_xs))}
+    logging.debug("Test Set dictionary \n" + re.sub("(.{82})", "\\1\n", str(testsetdict), 0, re.DOTALL) + "\n")
     
-    combinationset = ConcatDataset([training_set, test_set])
+    # endregion TODO - MOVE INTO DATASET1 CLASS
+
+    combinationset = ConcatDataset([trainingdict, testsetdict])
 
     kfold = KFold(n_splits=k_folds, shuffle=True)
 
     # Starting Model Training and Evaluation Here
     logging.debug("Starting model training and evaluation over " + str(kfold) + " cross-validation folds.")
 
-    # TODO - John, Eric, Dominic - finish K-fold cross-validation 
-
     for fold, (train, test) in enumerate(kfold.split(combinationset)):
-        print("to impliment")
-        print(train)
-        print(test)
-        logging.debug("Fold " + str(fold))
-
+        
         # Batch size = 
         train_subsampler = torch.utils.data.SubsetRandomSampler(train)
-        trainloader = DataLoader(combinationset, batch_size=1, sampler=train_subsampler)
+        trainloader = DataLoader(combinationset, batch_size=12, sampler=train_subsampler)
 
         test_subsampler = torch.utils.data.SubsetRandomSampler(test) 
-        testloader = DataLoader(combinationset, batch_size=1, sampler=test_subsampler)
+        testloader = DataLoader(combinationset, batch_size=12, sampler=test_subsampler)
 
         model = CropYieldPredictionModel()
         model.apply(reset_weights)
-        optimizer = torch.optim.Adam(model.parameters(), lr=2e-4)
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+        print("Training Model for Fold "+ str(fold))
+        logging.debug("Training Model for Fold "+ str(fold))
 
         for epoch in range(0, training_epochs):
-            
-            logging.debug("########## Epoch " + str(epoch+1) + " ##########")
+            print("Fold - " + str(fold) +  " Epoch - " + str(epoch+1))
+            logging.debug("########## Fold - " + str(fold) +  " Epoch - " + str(epoch+1) + " ##########")
             current_loss = 0.0
-
-            print(len(combinationset))
-            print(len(trainloader))
-            print(len(testloader))
-
-            print(next(iter(trainloader)))
 
             for i, data in enumerate(trainloader, 0): # THIS LINE CAUSING AN ERROR FROM TRAINLOADER
 
-                print(i)
-
-                inputs, target = data
+                inputs = data["x"]
+                inputs = inputs.unsqueeze(1)
+                targets = data["y"]
+                # print(i)
+                # print(data)
+                # print(inputs)
+                # print(targets)
                 if useCUDA:
-                    inputs, target = inputs.cuda(), target.cuda()
+                    inputs, targets = inputs.cuda(), targets.cuda()
 
                 optimizer.zero_grad()
+                
+                #print(targets)
 
                 output = model(inputs)
+                #print(output)
 
-                loss = loss_function(output, target)
+                loss = loss_function(output, targets)
 
                 loss.backward()
 
@@ -215,42 +270,61 @@ if __name__ == "__main__":
 
                 current_loss += loss.item()
                 if i % 5 == 0:
-                    print("Loss after mini-batch" + str(i+1) + "-" + str(current_loss / 5))
+                    logging.debug("RMSE Loss after batch member " + str(i+1) + " = " + str(current_loss / 5))
                     current_loss = 0.0
 
+        print("Training Model for Fold "+ str(fold)+" completed, saving model.")
+        logging.debug("Training Model for Fold "+ str(fold)+" completed, saving model.")
 
-
-
-
-    exit()
-
-    # Training Set
-    DLTrain = torch.utils.data.DataLoader(DS, batch_size=12, shuffle=True)
-
-    # Test Set
-    DLTest = torch.utils.data.DataLoader(DS, batch_size=12, shuffle=True)
-
-    
-    
-    
-
-    # Model Training
-    model.train()
-    for epoch in range(training_epochs):
-        for X_batch, y_batch in DLT:
-            if useCUDA:
-                X_batch, y_batch = X_batch.cuda(), y_batch.cuda()
-            y_pred = model(X_batch)
-            loss = loss_fn(y_pred, y_batch)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        save_path = resdir+"/model-fold-"+str(fold)+".pth"
+        torch.save(model.state_dict(), save_path)
         
+        print("Testing Model for Fold "+ str(fold))
+        logging.debug("Testing Model for Fold "+ str(fold))
+
+        # TODO - John, Eric, Dominic - finish K-fold cross-validation 05/18/2023 DO IT HERE BOI fix it up
+
+        # Evaluation for this fold
+        correct, total = 0, 0
+        with torch.no_grad():
+            # Iterate over the test data and generate predictions
+            for i, data in enumerate(testloader, 0):
+
+                # Get inputs
+                inputs = data["x"][0].unsqueeze(0)
+                inputs = inputs.unsqueeze(1)
+                targets = data["y"]
+
+                if useCUDA:
+                    inputs, targets = inputs.cuda(), targets.cuda()
+
+                # Generate outputs
+                outputs = model(inputs)
+
+                # Set total and correct
+                _, predicted = torch.max(outputs.data, 1)
+                total += targets.size(0)
+                correct += (predicted == targets).sum().item()
+
+            # Print accuracy
+            print('Accuracy for fold %d: %d %%' % (fold, 100.0 * correct / total))
+            print('--------------------------------')
+            results[fold] = 100.0 * (correct / total)
+
+    # Print fold results
+  
+    print(f'K-FOLD CROSS VALIDATION RESULTS FOR {k_folds} FOLDS')
+    print('--------------------------------')
+    sum = 0.0
+    for key, value in results.items():
+        print(f'Fold {key}: {value} %')
+        sum += value
+    print(f'Average: {sum/len(results.items())} %')
     
     # Test model
-    model.eval()
-    y_pred = model(X_test)
-    acc = (y_pred.round() == y_test).float().mean()
+    #model.eval()
+    #y_pred = model(X_test)
+    #acc = (y_pred.round() == y_test).float().mean()
 
 
 else:
